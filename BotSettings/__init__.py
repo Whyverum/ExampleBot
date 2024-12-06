eef856ed20a83aa4bfc9f4055501ca29582c6fb0
Whyverum <sergeyzavalin@outlook.com>
2024-12-06 21:35:50 +0700

    Новая структура инициализации

---
+++ BotSettings/__init__.py
@@ -0,0 +1,11 @@
+# BotSettings/__init__.py
+# Инициализация пакета BotSettings, для работы с базовыми функциями и экземплярами
+
+# Импортируем библиотеки для экспорта
+from .config import *
+from .bots import *
+from .logger import *
+
+from .start_info_out import *
+from .types_messages import *
+from .decorator import *
--- settings/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-from .config import *
-from .bots import *
-from .bot_logger import *
-
-from .bot_info_out import *
-from .types_message import *
-from .decorator import *
